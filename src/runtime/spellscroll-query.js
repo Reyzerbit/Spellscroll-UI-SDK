4d695f6ee56e673a2b790994b3f79835d45cca3b
Jacob Batista <jacobaccio@gmail.com>
2026-06-12 16:13:36 -0600

    More query stuffs

--- src/runtime/spellscroll-query.js
+++ src/runtime/spellscroll-query.js
@@ -4,11 +4,22 @@ function useSpellscrollMessaging()
 {
     if (!window?.spellscrollQuery) throw new Error('App is not running on a Spellscroll JCEF instance!');
 
-    return (action, data, onSuccess, onFailure) => window.spellscrollQuery({
+    const queryFunc = (action, data, onSuccess, onFailure) => window.spellscrollQuery({
         request:   JSON.stringify({ action, data }),
         onSuccess,
         onFailure,
     });
+
+    const supplierFunc = (action, onSuccess, onFailure) => window.spellscrollQuery({
+        request: JSON.stringify({ action }),
+        onSuccess,
+        onFailure,
+    });
+
+    return {
+        post: queryFunc,
+        get: supplierFunc
+    };
 }
 
 module.exports = { useSpellscrollMessaging };
